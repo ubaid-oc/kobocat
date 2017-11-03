463697d88d9e5c3b4556a29b534c71af89be4825
John N. Milner <john@tmoj.net>
2017-11-03 17:20:42 -0400

    !UNHCR ONLY! Consolidated UNHCR changes
    * Bypass `chown` on startup
    * Use `master` branch of vanilla XLSForm/pyxform

--- docker/sync_static.sh
+++ docker/sync_static.sh
@@ -11,9 +11,10 @@ mkdir -p "${KOBOCAT_SRC_DIR}/onadata/static"
 echo "Collecting static files..."
 python manage.py collectstatic -v 0 --noinput
 echo "Done"
-echo "Fixing permissions..."
-chown -R "${UWSGI_USER}" "${KOBOCAT_SRC_DIR}"
-echo "Done."
+# Bypassing slow `chown` for Shadi only
+#echo "Fixing permissions..."
+#chown -R "${UWSGI_USER}" "${KOBOCAT_SRC_DIR}"
+#echo "Done."
 echo "Syncing to nginx folder..."
 rsync -aq ${KOBOCAT_SRC_DIR}/onadata/static/* /srv/static/
 echo "Done"
